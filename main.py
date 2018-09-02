2267d343f46956e58d8090b38c9cc05ca7bf074f
Unknown <vejvarm@gmail.com>
2018-09-03 01:31:43 +0200

    variable SIZE changed to FIELD.rows and FIELD.cols named tuple
    the grid doesn't have to be square anymore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from collections import namedtuple
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -7,7 +8,8 @@ from kivy.uix.button import Button
 from kivy.properties import ListProperty
 from kivy.clock import Clock
 
-SIZE = 30
+field = namedtuple('field', ['rows', 'cols'])
+FIELD = field(30, 60)
 
 
 class UILayout(BoxLayout):
@@ -17,17 +19,17 @@ class UILayout(BoxLayout):
 
 
 class Grid(GridLayout):
-    cells = np.zeros((SIZE, SIZE), dtype='bool')
-    neighbours = {(i, j): 0 for i in range(SIZE) for j in range(SIZE)}
+    cells = np.zeros((FIELD.rows, FIELD.cols), dtype='bool')
+    neighbours = {(i, j): 0 for i in range(FIELD.rows) for j in range(FIELD.cols)}
     Colours = {False: (0.1, 0.1, 0.1, 1), True: (1, 0, 0, 1)}
     event = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.cols = SIZE
+        self.cols = FIELD.cols
         # fill the grid
-        for row in range(SIZE):
-            for col in range(SIZE):
+        for row in range(FIELD.rows):
+            for col in range(FIELD.cols):
                 self.grid_entry = GridEntry(coords=(row, col))
                 self.grid_entry.bind(on_release=self.button_pressed)
                 self.add_widget(self.grid_entry)
@@ -43,8 +45,8 @@ class Grid(GridLayout):
 
     def neighbourhoods(self):
         # update neighbours
-        for row in range(SIZE):
-            for col in range(SIZE):
+        for row in range(FIELD.rows):
+            for col in range(FIELD.cols):
                 if self.cells[row, col]:  # if cell is alive subtract 1
                     self.neighbours[(row, col)] = np.sum(
                         self.cells[max(row - 1, 0):row + 2, max(col - 1, 0):col + 2]) - 1
@@ -56,8 +58,8 @@ class Grid(GridLayout):
         self.neighbourhoods()
 
         # update the cell states in cells array
-        for row in range(SIZE):
-            for col in range(SIZE):
+        for row in range(FIELD.rows):
+            for col in range(FIELD.cols):
                 if self.neighbours[(row, col)] == 3:
                     self.cells[row, col] = True
                 elif self.cells[row, col] and self.neighbours[(row, col)] == 2:
@@ -66,22 +68,22 @@ class Grid(GridLayout):
                     self.cells[row, col] = False
 
                 # update the colour of the Cells depending on their state
-                self.children[-SIZE * row - col - 1].background_color = self.Colours[self.cells[row, col]]
+                self.children[-FIELD.cols * row - col - 1].background_color = self.Colours[self.cells[row, col]]
 
     def clear(self):
-        self.cells = np.zeros((SIZE, SIZE), dtype='bool')
-        self.neighbours = {(i, j): 0 for i in range(SIZE) for j in range(SIZE)}
+        self.cells = np.zeros((FIELD.rows, FIELD.cols), dtype='bool')
+        self.neighbours = {(i, j): 0 for i in range(FIELD.rows) for j in range(FIELD.cols)}
         # update the colour of the Cells depending on their state
         for child in self.children:
             child.background_color = self.Colours[False]
 
     def random(self):
-        self.cells = np.random.choice(a=[False, True], size=(SIZE, SIZE), p=[0.8, 0.2])
+        self.cells = np.random.choice(a=[False, True], size=(FIELD.rows, FIELD.cols), p=[0.8, 0.2])
 
         # update the colour of the Cells depending on their state
-        for row in range(SIZE):
-            for col in range(SIZE):
-                self.children[-SIZE * row - col - 1].background_color = self.Colours[self.cells[row, col]]
+        for row in range(FIELD.rows):
+            for col in range(FIELD.cols):
+                self.children[-FIELD.cols * row - col - 1].background_color = self.Colours[self.cells[row, col]]
 
     def auto_run(self):
         if not self.event and self.tgl.state == 'down':
